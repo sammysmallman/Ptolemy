56edfcf2441b8f195f303172fe43506432bfc65f
Sam Smallman <srsmallman@mac.com>
2020-09-20 09:54:19 +0100

    Initial Commit

---
+++ Ptolemy/PtolemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PtolemyApp.swift
+//  Ptolemy
+//
+//  Created by Sam Smallman on 20/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct PtolemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
